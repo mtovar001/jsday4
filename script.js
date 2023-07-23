666220b5553f54f2104e78ad0fc1a7a15a634064
Mildred <mtovar001@hotmail.com>
2023-07-23 11:03:15 -0400

    finishing hw

---
+++ script.js
@@ -0,0 +1,55 @@
+/*Q1: 
+1. Create an array called grades and put the following values: 56, 87, 99, 40
+2. Add another element at the end of the array with value: 68
+3. Remove the 1st element of the array
+4. Change 40 to 60.
+5. Create a variable called average and compute for the average of the grades array.
+    Tip: you can create another variable as a placeholder for the sum first.
+*/
+
+const grades = [56,87,99,40]; {
+console.log (grades);
+}
+let addremove = grades.splice (4,1,68);{
+    console.log (grades)
+}
+
+function replace(){
+    let grade = grades.replace(40,60);
+    console.log (grade);
+}
+
+
+
+
+
+    
+
+
+
+/*Q2:
+1. Create an object called vehicle with following properties:
+    a. name (String)
+    b. brand (String)
+    c. year of make (Number)
+    b. number of wheels (Number)
+    e. isEnvironmentFriendly (Boolean)
+Feel free to put any value.
+2. Use the for-in loop to display both the properties and values of the object.
+*/
+ 
+const vehicle = {
+    name: "NX",
+    brand: "LX",
+    yearofmake: 2020,
+    numberofwheels: 4,
+    isenvironmentFriendly: "yes",
+}
+
+let car ="";
+   for (let x in vehicle){
+    car += vehicle[x] + "<br>";
+    console.log (car)
+}
+
+   
